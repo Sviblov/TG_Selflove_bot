5f17ba9489726d938b5640fbc2a00819ef397b33
Sviblov <igor.fedotov89@gmail.com>
2024-01-13 13:57:21 +0000

    deploy change

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,14 @@ sudo docker stop selflove_bot
 
 sudo docker container rm selflove_bot
 
+sudo docker image rm selflove
+
 git -C /home/ubuntu/TG_Selflove_bot pull
 
 #up containers again
 
-sudo docker build -t selflove -f /home/ubuntu/TG_Selflove_bot .
+cd /home/ubuntu/TG_Selflove_bot
+sudo docker build -t selflove .
+cd ..
 
 sudo docker run -d --name selflove_bot selflove
