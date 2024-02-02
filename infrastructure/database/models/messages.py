9fbd8640fb060e874026d44b89796ee948192872
Sviblov <igor.fedotov89@gmail.com>
2024-02-02 23:19:32 +0000

    DB updated

--- infrastructure/database/models/messages.py
+++ infrastructure/database/models/messages.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.sql.functions import func
 import datetime
-
+from sqlalchemy.dialects.postgresql import TEXT
 
 from .base import Base
 
@@ -21,7 +21,7 @@ class message(Base):
     message_type: Mapped[str] = mapped_column(String(16))
     user_from: Mapped[int] = mapped_column(ForeignKey("users.user_id"))
     user_to: Mapped[int] = mapped_column(ForeignKey("users.user_id"))
-    text: Mapped[str]  = mapped_column(String(256),nullable=True)
+    text: Mapped[str]  = mapped_column(TEXT,nullable=True)
     sent_at: Mapped[datetime] = mapped_column(TIMESTAMP, server_default=func.now())
 
     def __repr__(self):
