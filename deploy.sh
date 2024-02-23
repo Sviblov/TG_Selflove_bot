611c9cd37850ed89b6c2631d6bdd424891e32f46
Sviblov <igor.fedotov89@gmail.com>
2024-02-23 00:34:41 +0000

    backend added to the project

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-sudo docker stop selflove_bot
-
-sudo docker container rm selflove_bot
-
-sudo docker image rm selflove
+sudo docker-compose down -f /home/ubuntu/TG_Selflove_bot/docker-compose.yml
 
 git -C /home/ubuntu/TG_Selflove_bot pull
 
 #up containers again
 
-cd /home/ubuntu/TG_Selflove_bot
-sudo docker build -t selflove .
-cd ..
-
-sudo docker run -d --name selflove_bot selflove
+sudo docker-compose up --build -d -f /home/ubuntu/TG_Selflove_bot/docker-compose.yml
